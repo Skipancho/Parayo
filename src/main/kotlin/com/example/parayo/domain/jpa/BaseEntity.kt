25152091f9e971cc9576960b6ab71c5dfa84d79f
Skipancho <ctg1770a@gmail.com>
2021-12-11 16:26:04 +0900

    + 주석 추가

--- src/main/kotlin/com/example/parayo/domain/jpa/BaseEntity.kt
+++ src/main/kotlin/com/example/parayo/domain/jpa/BaseEntity.kt
@@ -5,6 +5,12 @@ import javax.persistence.*
 
 @MappedSuperclass
 abstract class BaseEntity {
+    /**
+     * @Id 애노테이션은 해당 필드가 이 테이블의 PK(Primary Key)라는 것을 명시해준다.
+     * @GeneratedValue(strategy = GenerationType.IDENTITY) 애노테이션은 MySQL에
+     * PK 생성을 위임해 테이블에 새 데이터가 저장될 때 해당 필드가 자동으로 1씩 증가하여 유일한 값을
+     * 넣어주도록 한다.(AUTO_INCREMENT)
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id : Long? = null
@@ -12,12 +18,20 @@ abstract class BaseEntity {
     var createAt: Date? = null
     var updateAt: Date? = null
 
+    /**
+     * @PrePersist가 붙은 함수는 데이터베이스에 새 데이터가 저장되기 전에 자동으로 호출
+     * 데이터 저장 전에 데이터 생성일을 현재 날짜로 지정
+     */
     @PrePersist
     fun prePersist(){
         createAt = Date()
         updateAt = Date()
     }
 
+    /**
+     * @PreUpdate 애노테이션은 @PrePersist와 마찬가지로 JPA의 라이프사이클 훅을 지정한다.
+     * 해당 애노테이션이 붙은 함수는 데이터베이스에 데이터 업데이트 명령을 날리기 전에 실행된다.
+     */
     @PreUpdate
     fun preUpdate(){
         updateAt = Date()
--- src/main/kotlin/com/example/parayo/domain/user/User.kt
+++ src/main/kotlin/com/example/parayo/domain/user/User.kt
@@ -4,6 +4,10 @@ import com.example.parayo.domain.jpa.BaseEntity
 import java.util.*
 import javax.persistence.*
 
+/**
+ * @Entity 애노테이션은 이 클래스가 테이블에 맵핑된 정보를 가지고 있음을 의미함
+ * User 클래스의 객체가 가진 데이터는 데이터베이스에 저장된 혹은 저장될 사용자 한 명의 정보를 대변한다.
+ */
 @Entity(name = "user")
 class User(
     var email : String,
