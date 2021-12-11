9e4188ce0d159a9db6143e49228e6e9719d2681d
Skipancho <ctg1770a@gmail.com>
2021-12-11 17:27:00 +0900

    + 주석 추가

--- src/main/kotlin/com/example/parayo/common/ParayoExceptionHandler.kt
+++ src/main/kotlin/com/example/parayo/common/ParayoExceptionHandler.kt
@@ -5,6 +5,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * @ControllerAdvice 애노테이션은 Spring에게 이 클래스가
+ * 전역적인 익셉션 핸들러임을 알려준다.
+ *
+ * @ExceptionHandler(...) 애노테이션은 이 함수가 괄호 안에 들어간 타입의
+ * 예외를 처리할 것이라는 것을 알려준다.
+ */
 @ControllerAdvice
 @RestController
 class ParayoExceptionHandler {
--- src/main/kotlin/com/example/parayo/controller/UserApiController.kt
+++ src/main/kotlin/com/example/parayo/controller/UserApiController.kt
@@ -8,6 +8,19 @@ import com.example.parayo.domain.user.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
+/**
+ * @RestController 애노테이션이 달린 컨트롤러는 Spring에서
+ * HTTP 호출의 응답으로 뷰를 렌더링하지 않고 HTTP의 본문에 직접 텍스트로
+ * 이루어진 데이터를 쓴다는 것을 나타낸다.
+ *
+ * @GetMapping 애노테이션이 붙은 함수는 HTTP의 GET 메서드를 통해
+ * 해당 함수를 실행하면 이 반환값을 응답으로 되돌려준다는 의미이다.
+ *
+ * @PostMapping 애노테이션은 HTTP의 POST 메서드를 이용해 매핑된 주소를
+ * 호출했을 때 함수가 동작함을 나타낸다.
+ *
+ * @RequestBody 애노테이션은 데이터를 HTTP의 바디에서 읽는다는 것을 의미한다.
+ */
 @RestController
 @RequestMapping("/api/v1")
 class UserApiController @Autowired constructor(
