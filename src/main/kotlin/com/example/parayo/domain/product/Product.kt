002dc41074f86631253ab36d73f403b329a7a0cc
Skipancho <ctg1770a@gmail.com>
2021-12-12 16:41:21 +0900

    + 주석 추가

--- src/main/kotlin/com/example/parayo/domain/product/Product.kt
+++ src/main/kotlin/com/example/parayo/domain/product/Product.kt
@@ -4,6 +4,20 @@ import com.example.parayo.domain.jpa.BaseEntity
 import java.util.*
 import javax.persistence.*
 
+/**
+ * @Column 애노테이션은 해당 프로퍼티가 데이터베이스의 컬럼에 매핑될 때의 속성들을 지정
+ * 컬럼의 이름, 최대 길이, 유일 키 여부 등 여러 옵션을 지정할 수 있다.
+ *
+ * @Enumerated 애노테이션은 enum 클래스가 데이터베이스에 어떤 형식으로 저장되어야 할지를
+ * 지정한다. 디폴트는 ORDINAL 로 정수 형태로 저장된다.
+ * EnumType.STRING 시 코드 상의 문자 그대로 저장됨
+ *
+ * @OneToMany 애노테이션은 1:M 관계의 매핑을 의미한다.
+ * 따라서 해당 프로퍼티는 MutableList 타입이어야함
+ *
+ * @JoinColumn 애노테이션은 해당 엔티티의 PK와 매핑되는 타겟 엔티티의 어떤 컬럼을 통해
+ * 두 테이블을 조인할 것인가를 지정한다. 이 애노테이션을 통해 FK(외래키)를 추가해 조인 쿼리를 사용
+ */
 @Entity(name = "product")
 class Product(
     @Column(length = 40)
--- src/main/kotlin/com/example/parayo/interceptor/TokenValidationInterceptor.kt
+++ src/main/kotlin/com/example/parayo/interceptor/TokenValidationInterceptor.kt
@@ -10,10 +10,18 @@ import org.springframework.web.servlet.ModelAndView
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+/**
+ * @Component 애노테이션은 이 클래스가 Spring이 관리하는 빈(Bean) 클래스임을 나타낸다.
+ * @Service 애노테이션과 기술적으로는 동일하나 의미상으로 비즈니스 로직을 처리하는
+ * 클래스가 아니라는 점이 다르다.
+ */
 @Component
 class TokenValidationInterceptor @Autowired constructor(
     private val userContextHolder: UserContextHolder
 ) : HandlerInterceptor {
+    /**
+     * 서버사이드 로깅을 위해 로거 객체를 프로퍼티로 선언
+     */
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun preHandle(
@@ -69,7 +77,12 @@ class TokenValidationInterceptor @Autowired constructor(
         token.replace(BEARER, "").trim()
 
 
-
+    /**
+     * AUTHORIZATION : Authorization 토큰이 포함된 헤더 값을 가져오기 위한 상수
+     * BEARER : Authorization 헤더에 JWT 토큰을 전달할 때 사용되는 인증 방법
+     *          실제 토큰을 사용하려면 헤더 값에서 이 문자열을 제거한 후 사용
+     * DEFAULT_ALLOWED_API_URLS : 토큰 인증 없이 사용할 수 있는 URL 을 정의
+     */
     companion object{
         private const val AUTHORIZATION = "Authorization"
         private const val BEARER = "Bearer"
