69e5fec4bcc819e54cb96e9343af00d4a5a88b04
Skipancho <ctg1770a@gmail.com>
2021-12-12 16:56:32 +0900

    + 주석 추가

--- src/main/kotlin/com/example/parayo/config/WebConfig.kt
+++ src/main/kotlin/com/example/parayo/config/WebConfig.kt
@@ -7,6 +7,11 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+/**
+ * @Configuration 애노테이션은 이 클래스가 Spring에서 사용하는 설정을 담은
+ * 빈(Bean) 클래스임을 나타낸다. 이 애노테이션을 사용하면 SpringBootApplication 이하의
+ * 패키지에서 모든 설정 클래스들을 검사해 자동으로 빈을 생성해준다.
+ */
 @Configuration
 class WebConfig @Autowired constructor(
     private val tokenValidationInterceptor: TokenValidationInterceptor
